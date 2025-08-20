922598afe9782abf1ad8e4bbcf72fa0b0d4d86c0
Xnoch3 <xaviernochelli@gmail.com>
2025-08-19 22:00:23 -0300

    se hacen arreglos en el Token en Getters y Setters

--- src/main/java/com/Alura/ChallengeForo/models/Topico.java
+++ src/main/java/com/Alura/ChallengeForo/models/Topico.java
@@ -34,4 +34,53 @@ public class Topico {
 
     @NotBlank
     private String curso;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
 }
--- src/main/java/com/Alura/ChallengeForo/models/Usuario.java
+++ src/main/java/com/Alura/ChallengeForo/models/Usuario.java
@@ -28,4 +28,46 @@ public class Usuario {
     private String password;
 
     private Boolean activo = true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+
 }
